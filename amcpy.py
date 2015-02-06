5ad1ead9622fe1f2f341676042e4dc60c1b1fde2
Zhechen.Zhu@gmail.com <Zhechen.Zhu@gmail.com>
2015-02-06 16:04:03 +0000

    Initial commit

---
+++ amcpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def amcml(sigIn,modulationPool,channelParameter):
+    "AMCML classifies the modulation type of the input signal using the maxium\
+            likelihood classifier."
+    
+
+    :sigIn: TODO
+    :modulationPool: TODO
+    :channelParameter: TODO
+    :returns: TODO
+
+    """
+    pass
